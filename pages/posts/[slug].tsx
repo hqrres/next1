fd799f063f7608da6308297882bbbfb93c60129a
hq <hq.v.hegares@hqrres.com>
2024-03-28 21:01:52 +0200

    added header footer to post view

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,33 +1,40 @@
 import { GetStaticProps } from "next";
 import { GetStaticPaths } from "next";
 
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+
 import { getPosts, getPostBySlug } from "@/lib/service";
 
 export default function PostDetails({ post }: { post: any }) {
   return (
-    <section className="container mx-auto py-12">
-      <div
-        className="post-header relative flex flex-col items-center justify-center w-full min-h-[200px] rounded-md"
-        style={{
-          backgroundImage: `url(${post.featuredImage.node.sourceUrl})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
+    <>
+      <Header />
+      <section className="container mx-auto py-12">
         <div
-          className="absolute w-full h-full z-10"
-          style={{ backgroundColor: "rgba(0, 0, 0, .5)" }}
-        ></div>
-        <div className="z-20 text-center">
-          <h1 className="text-2xl md:text-4xl mb-4">{post.title}</h1>
-          <p className="italic">By Jeffrey</p>
+          className="post-header relative flex flex-col items-center justify-center w-full min-h-[200px] rounded-md"
+          style={{
+            backgroundImage: `url(${post.featuredImage.node.sourceUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div
+            className="absolute w-full h-full z-10"
+            style={{ backgroundColor: "rgba(0, 0, 0, .5)" }}
+          ></div>
+          <div className="z-20 text-center">
+            <h1 className="text-2xl md:text-4xl mb-4">{post.title}</h1>
+            <p className="italic">By Jeffrey</p>
+          </div>
         </div>
-      </div>
-      <div
-        className="post-content w-full md:w-3/5 mx-auto mt-20 py-6 text-lg"
-        dangerouslySetInnerHTML={{ __html: post.content }}
-      ></div>
-    </section>
+        <div
+          className="post-content w-full md:w-3/5 mx-auto mt-20 py-6 text-lg"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        ></div>
+      </section>
+      <Footer />
+    </>
   );
 }
 
