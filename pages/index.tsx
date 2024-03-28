052f7701e2ea74ea222ffa00ad8adadb54e1ba18
hq <hq.v.hegares@hqrres.com>
2024-03-28 16:43:20 +0200

    update .env content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,108 @@
-import Link from 'next/link'
+//import Link from 'next/link'
 
-export default function Home({posts}) {
-  
-  //console.log(posts);
+import { GetStaticProps } from "next";
 
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import { Hero } from "@/components/Hero";
+import { PostBlock } from "@/components/PostBlock";
+import { getPosts } from "@/lib/service";
+
+export default function HomePage({ posts }: { posts: any }) {
   return (
+    <>
+      <Header />
+      <Hero />
+      <div className="container mx-auto py-8">
+        <h3 className="text-xl">All my posts ({posts.length})</h3>
+        <div className="my-6 grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          {posts.map((post: any) => {
+            return <PostBlock key={post.slug} post={post} />;
+          })}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts(100); // retrieve first 100 posts
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 3600,
+  };
+};
+
+
+
+// export default function Home({posts}) {
+  
+//   //console.log(posts);
+
+//   return (
     
-    <div>
-      <h1>Hello From The Home Page!</h1>
-      {
-        posts.nodes.map(post => {
-          return(
-            <ul key={post.slug}>
-              <li>
-                <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-              </li>
-            </ul>
-          )
-        })
-      }
-    </div>
-  )
+//     <div>
+//       <h1>Hello From The Home Page!</h1>
+//       {
+//         posts.nodes.map(post => {
+//           return(
+//             <ul key={post.slug}>
+//               <li>
+//                 <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+//               </li>
+//             </ul>
+//           )
+//         })
+//       }
+//     </div>
+//   )
 
-}
+// }
 
-export async function getStaticProps(){
 
- const wp_graphql = process.env.WP_GRAPHQL
+// export const getStaticProps: GetStaticProps = async () => {
+//   const posts = await getPosts(100); // retrieve first 100 posts
 
- const res = await fetch( wp_graphql, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    query: `
-    query HomePageQuery {
-      posts {
-        nodes {
-          slug
-          title
-        }
-      }
-    }
-    `,  
-  })
+//   return {
+//     props: {
+//       posts,
+//     },
+//     revalidate: 3600,
+//   };
+// };
 
- }) 
 
- const json = await res.json();
+// export async function getStaticProps(){
 
- return {
-  props: {
-    posts: json.data.posts,
-  }
- };
-}
+//  const wp_graphql = process.env.WP_GRAPHQL
+
+//  const res = await fetch( wp_graphql, {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify({
+//     query: `
+//     query HomePageQuery {
+//       posts {
+//         nodes {
+//           slug
+//           title
+//         }
+//       }
+//     }
+//     `,  
+//   })
+
+//  }) 
+
+//  const json = await res.json();
+
+//  return {
+//   props: {
+//     posts: json.data.posts,
+//   }
+//  };
+// }
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,138 @@
+import { GetStaticProps } from "next";
+
+import { getPosts, getPostBySlug } from "@/lib/service";
+
+export default function PostDetails({ post }: { post: any }) {
+  return (
+    <section className="container mx-auto py-12">
+      <div
+        className="post-header relative flex flex-col items-center justify-center w-full min-h-[200px] rounded-md"
+        style={{
+          backgroundImage: `url(${post.featuredImage.node.sourceUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div
+          className="absolute w-full h-full z-10"
+          style={{ backgroundColor: "rgba(0, 0, 0, .5)" }}
+        ></div>
+        <div className="z-20 text-center">
+          <h1 className="text-2xl md:text-4xl mb-4">{post.title}</h1>
+          <p className="italic">By Jeffrey</p>
+        </div>
+      </div>
+      <div
+        className="post-content w-full md:w-3/5 mx-auto mt-20 py-6 text-lg"
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      ></div>
+    </section>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getPosts(100); // retrieve first 100 posts
+
+  return {
+    paths: posts.map((post: any) => `/posts/${post.slug}`),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const post = await getPostBySlug(params?.slug as string);
+
+  return {
+    props: { post },
+  };
+};
+// import Image from 'next/image'
+
+
+// export default function Post( data ) {
+
+//     console.log({data})
+
+//     const post = data.post;
+
+//     return (
+//         <div>
+//             <h1>{post.title}</h1>
+//             <Image width="140" height="126" src={post.featuredImage.node.sourceUrl} alt=""/>
+//             <article dangerouslySetInnerHTML={{__html: post.content}}></article>
+//         </div>        
+//     )
+
+// }
+
+// export async function getStaticProps(context) {
+
+//     const res = await fetch('http://next1.local/graphql', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//             query: `
+//                 query SinglePost($id: ID!, $idType: PostIdType!) {
+//                 post(id: $id, idType: $idType) {
+//                         title
+//                         slug
+//                         content
+//                         featuredImage {
+//                             node {
+//                                 sourceUrl
+//                             }
+//                         }
+//                     }
+//                 }
+//             `,
+//             variables: {
+//                 id: context.params.slug,
+//                 idType: 'SLUG'
+//             }
+//         }) 
+//     })
+
+//     const json = await res.json()
+
+//     return {
+//         props: {
+//             post: json.data.post,
+//         }
+//     }
+
+// }
+
+// export async function getStaticPaths() {
+
+//     const res = await fetch('http://next1.local/graphql', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//             query: `
+//                 query AllPostsQuery {
+//                     posts {
+//                         nodes {
+//                             slug
+//                             content
+//                             title
+//                             featuredImage {
+//                                 node {
+//                                     sourceUrl
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             `
+//         })
+//     })
+
+//     const json = await res.json()
+//     const posts = json.data.posts.nodes;
+
+//     const paths = posts.map((post) => ({
+//         params: { slug: post.slug },
+//     }))
+
+//     return { paths, fallback: false }
+// }
